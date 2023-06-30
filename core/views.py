179e91eec8c3ae0b3a3d18529c7c895fd00e24b2
Andrey Smirnov <andreysmirnov@me.com>
2023-06-30 15:32:54 +0300

    add comments on home page
    left to do:
    - expansion of comment by 'more' link
    - style for comment picture

--- core/views.py
+++ core/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
-from catalog.models import CassetteCategory, Cassette
+from catalog.models import CassetteCategory, Cassette, CassetteComment
 from users.models import User
 
 
 def homepage(request):
     context = {
         'collector_list':User.objects.all()[:5],
-        'cassette_list': Cassette.objects.all()[:7] ,
+        'cassette_list': Cassette.objects.all()[:7],
         'category_list': CassetteCategory.objects.all(),
+        'comment_list': CassetteComment.objects.all()[:10],
     }
     return render(request, 'core/index.html', context)
-
-
