085364082ef1d33f84fd5dd2ee4f8ce09e6f0cd6
Andrey Smirnov <aevsmirnov@gmail.com>
2023-09-23 06:24:04 +0300

    fix: misspelling in wishlist

--- collection_management/urls.py
+++ collection_management/urls.py
@@ -5,7 +5,7 @@ app_name = 'lists'
 
 urlpatterns = [
     path('collection/<int:collector_id>/', CollectionListView.as_view(), name='collection'),
-    path('whishlist/<int:collector_id>/', WishlistListView.as_view(), name='whishlist'),
+    path('wishlist/<int:collector_id>/', WishlistListView.as_view(), name='wishlist'),
     path('exchange/<int:collector_id>/', ExchangeListView.as_view(), name='exchange'),
     path('sale/<int:collector_id>/', SaleListView.as_view(), name='sale'),
 ]
